f5b24d2e97130e0b3c647c5f53ca0b5c568b65c2
Ian <naberbach@gmail.com>
2022-11-20 15:27:12 -0500

    finished

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -4,6 +4,7 @@ import { CurrentUser } from "../contexts/CurrentUser";
 import CommentCard from './CommentCard'
 import NewCommentForm from "./NewCommentForm";
 
+
 function PlaceDetails() {
 
 	const { placeId } = useParams()
@@ -54,8 +55,7 @@ function PlaceDetails() {
 		const response = await fetch(`http://localhost:5000/places/${place.placeId}/comments`, {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json',
-				'Authorization': `Bearer ${localStorage.getItem('token')}`
+				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(commentAttributes)
 		})
@@ -105,6 +105,22 @@ function PlaceDetails() {
 		})
 	}
 
+
+	let placeActions = null
+
+	if (currentUser?.role === 'admin') {
+		placeActions = (
+			<>
+				<a className="btn btn-warning" onClick={editPlace}>
+					Edit
+				</a>{` `}
+				<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+					Delete
+				</button>
+			</>
+		)
+	}
+
 	return (
 		<main>
 			<div className="row">
@@ -131,12 +147,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{placeActions}
 				</div>
 			</div>
 			<hr />
